c8b61417f00d9dc749182292898d09f30babfabf
legeonerkd <legeonerkd@gmail.com>
2022-03-01 22:26:31 +0200

    added function add articles

--- server.py
+++ server.py
@@ -1,3 +1,16 @@
 from flask import Flask, request, render_template
+from flask_pymongo import PyMongo
 import json
+import flask
 
+
+app = Flask(__name__, static_url_path='/static')
+app.config["MONGO_URI"] = "mongodb://27017:27017/shop"
+shop = PyMongo(app)
+shop_db = shop.db
+
+
+@app.route("/add_article")
+def add_artecle():
+    shop_db.articles.insert_one({'name': "todo title", 'price': "todo body", 'description': ""})
+    return flask.jsonify(message="success")
